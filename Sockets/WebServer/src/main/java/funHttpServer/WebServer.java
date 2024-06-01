5cc17425dd1686a5e578f792b5cf05ef5a055e9e
aelevi1 <105684442+aelevi1@users.noreply.github.com>
2024-06-01 10:38:15 -0700

    Correct value error.

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -200,16 +200,16 @@ class WebServer {
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
+          
+          // extract required fields from parameters
+          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
 
           try {
             //checking to see if both parameters are present
             if(!query_pairs.containsKey(num1) || !query_pairs.containsKey(num2)){
               throw new IllegalArgumentException("Missing parameter.  Please provide both num1 and num2.");
             }
-          
-          // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
 
           // do math
           Integer result = num1 * num2;
