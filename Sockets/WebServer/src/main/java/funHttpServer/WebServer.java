3a9a37fc6b444d2a65d3d965ae6bf2bdad2346db
aelevi1 <105684442+aelevi1@users.noreply.github.com>
2024-06-01 13:37:54 -0700

    Correct value error.

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -231,6 +231,7 @@ class WebServer {
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n"); 
+            builder.append("Error: " + e.getMessage());
           } 
             
         } else if (request.contains("github?")) {
